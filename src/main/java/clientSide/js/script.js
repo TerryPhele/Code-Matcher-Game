848ebf7f09baf44cd2fd30f2ab324484cf439608
t. phele <tphele023a@student.wethinkcode.co.za>
2024-03-15 13:16:46 +0200

    completed the project

--- src/main/java/clientSide/js/script.js
+++ src/main/java/clientSide/js/script.js
@@ -1,5 +1,7 @@
 const code =[];
 let currentLevel=1;
+DEFAULT_MIN = 3;
+DEFAULT_SEC = 30;
 let minutes=0;
 let seconds=0;
 let playing = false;
@@ -10,6 +12,19 @@ const level = document.querySelector("#level");
 const time = document.querySelector("#timeelapser");
 const playPause = document.querySelector("#playpause");
 
+const textOne = document.querySelector("#text1");
+const textTwo = document.querySelector("#text2");
+const textThree = document.querySelector("#text3");
+const textFour = document.querySelector("#text4");
+
+const lockOne = document.querySelector("#lock1");
+const lockTwo = document.querySelector("#lock2");
+const lockThree = document.querySelector("#lock3");
+const lockFour = document.querySelector("#lock4");
+
+const reset=document.querySelector("#reset");
+const unlock = document.querySelector("#next");
+const exit = document.querySelector("#exit")
 
 
 function updatePage(){
@@ -25,6 +40,9 @@ function updatePage(){
     if(seconds < 10){
         displaySeconds = `0${seconds}`;
     }
+    if( minutes ==0 && seconds <=59){
+        time.style.color ="red";
+    }
 
     time.textContent =`${ displayMinutes }:${displaySeconds}`;
 }
@@ -35,8 +53,8 @@ function updatePage(){
 function timeCounter(){
 
    if(!gameStarted){
-      minutes = 3;
-      seconds = 30;
+      minutes = DEFAULT_MIN;
+      seconds = DEFAULT_SEC;
    }
 
     if(playing){
@@ -87,10 +105,17 @@ function playOrPause()
 
         playPause.value ="Pause";
         playing = pauseTime(true);
+        textOne.disabled = false;
+        textTwo.disabled = false;
+        textThree.disabled = false;
+        textFour.disabled = false;
     } else{
         playPause.value = "Play";
         playing = pauseTime(false)
-        
+        textOne.disabled = true;
+        textTwo.disabled = true;
+        textThree.disabled = true;
+        textFour.disabled = true;
     }
 
     timeCounter();
@@ -99,3 +124,110 @@ function playOrPause()
 }
 playPause.addEventListener('click', playOrPause)
 
+
+function generateCode(){
+
+    textOne.disabled = true;
+    textTwo.disabled = true;
+    textThree.disabled = true;
+    textFour.disabled = true;
+    for(index = 0; index<4; index++){
+        let digit = Math.floor( Math.random()*5)+1;
+        code.push(digit);
+    }
+}
+
+function unlockOne(){
+    if( Number(textOne.value) === code[0]){
+        lockOne.src ="unlocked.png";
+    }else{
+        lockOne.src ="locked.png";
+    }
+}
+
+function unlockTwo(){
+    if( Number(textTwo.value) === code[1]){
+        lockTwo.src ="unlocked.png";
+    }else{
+        lockTwo.src ="locked.png";
+    }
+}
+function unlockThree(){
+    if( Number(textThree.value) === code[2]){
+        lockThree.src ="unlocked.png";
+    }else{
+        lockThree.src ="locked.png";
+    }
+}
+
+function unlockFour(){
+    if( Number(textFour.value) === code[3]){
+        lockFour.src ="unlocked.png";
+    }else{
+        lockFour.src ="locked.png";
+    }
+}
+
+
+
+
+
+textOne.addEventListener('input', unlockOne);
+textTwo.addEventListener('input', unlockTwo);
+textThree.addEventListener('input', unlockThree);
+textFour.addEventListener('input', unlockFour);
+
+reset.addEventListener('click', reseter);
+
+function reseter()
+{
+    textOne.value = "";
+    textTwo.value = "";
+    textThree.value = "";
+    textFour.value = "";
+    minutes = DEFAULT_MIN;
+    seconds = DEFAULT_SEC;
+
+    clearInterval(timer);
+    gameStarted = false;
+    playPause.value = "Play";
+
+    textOne.disabled = true;
+    textTwo.disabled = true;
+    textThree.disabled = true;
+    textFour.disabled = true;
+
+    lockOne.src ="locked.png";
+    lockTwo.src ="locked.png";
+    lockThree.src ="locked.png";
+    lockFour.src ="locked.png";
+    code.length = 0;
+    
+    updatePage();
+}
+
+unlock.addEventListener('click', ()=>{
+    if( unlocked()){
+        alert(`congrats you unlocked level-${currentLevel}\n
+        You are now moving to ${currentLevel+1}`)
+        
+        currentLevel+=1;
+        reseter();
+        generateCode();
+    }else{
+        alert("The code is still locked!!!")
+    }
+})
+
+exit.addEventListener('click', ()=>{
+    alert("GAME IS CLOSING BY!!");
+    window.close();
+})
+
+
+function unlocked(){
+    return textOne.value ==code[0] && textTwo.value ==code[1] &&
+            textThree.value==code[2] && textFour.value==code[3];
+}
+
+generateCode();
